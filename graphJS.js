73040e1a2f6df7d397d939d68502a660da904253
Patrick Mahler <patrick_mahler@web.de>
2019-11-30 16:20:56 +0100

    Graph mit Werten aus local Storage

--- graphJS.js
+++ graphJS.js
@@ -1,25 +1,20 @@
 window.onload = function () {
     console.log('Dokument geladen');
     
-
     let confirmButton = document.getElementById("confirm-btn");
     let permanentStorage = window.localStorage;
     let tempStorage = window.sessionStorage;
 
     // Einmalig die Session aus dem Local Storage holen
     let session = window.localStorage.getItem("session");
-    console.log(session);
     let sessionJSON = JSON.parse(session);
-    console.log(sessionJSON);
+
     // Variablen befüllen
-    let age = sessionJSON.age;
-    console.log(age);
-    // let weight
+    setVariables();
     
+    //Graph erzeugen
     setGraph ();
 }
-
-
 
 // ------------ Ausserhalb der Onload Funktion --------------------------
 
@@ -33,7 +28,7 @@ function setGraph () {
         data: {
             labels: ['Kaffee', 'Tee', 'Energy Drink', 'Cola', 'Mate'],
             datasets: [{
-                data: [10, 10, 4, 2, 8],
+                data: [kaffeeMenge,teeMenge, energyMenge, colaMenge, mateMenge],
                 backgroundColor:'rgba(255, 0, 0, 0.25)',
                 borderColor: 'rgba(128, 0, 0, 0.25)',
                 pointBackgroundColor: 'rgba(0, 0, 0, 0.25)',
@@ -42,6 +37,15 @@ function setGraph () {
             }]
         }
     });
+}
+
+// Funktion zum Befüllen der Variablen
+function setVariables () {
+    let kaffeeMenge = JSON.parse(window.localStorage,getItem("kaffee"));
+    let teeMenge    = JSON.parse(window.localStorage,getItem("tee"));
+    let energyMenge = JSON.parse(window.localStorage,getItem("energy"));
+    let colaMenge   = JSON.parse(window.localStorage,getItem("cola"));
+    let mateMenge   = JSON.parse(window.localStorage,getItem("mate"));
 }
 
 
